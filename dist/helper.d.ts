555166cc2b4b90be544f3e493c0aadbda9bfa245
gabaum10 <gabaum10@gmail.com>
2024-11-08 17:42:52 -0500

    add dist

---
+++ dist/helper.d.ts
@@ -0,0 +1,3 @@
+export declare function loadHtmlWebpackPluginIfInstalled(): Promise<typeof import("html-webpack-plugin")>;
+export declare const removeHash: (filename: string) => string;
+export declare const isFileHashed: (filename: string) => boolean;
